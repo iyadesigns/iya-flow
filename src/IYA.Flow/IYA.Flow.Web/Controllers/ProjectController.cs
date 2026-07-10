b3e80a720ff848bc9a1de0a2ff47307bfc1fa01a
IYA🦋 <iya.dev714@gmail.com>
2026-07-10 15:47:47 +0200

    Create project skeleton

---
+++ src/IYA.Flow/IYA.Flow.Web/Controllers/ProjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IYA.Flow.Web.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
